e82ae75132a1ce60aaeeaa5546e15afd2a5bbb16
Sebastian <sebaramirezx2010@gmail.com>
2023-03-16 23:20:41 -0300

    LocalStorage funcionando

--- index.js
+++ index.js
@@ -1,79 +1,96 @@
 const form = document.querySelector("form");
 const tareaVacia = document.querySelector("#vacio");
-
 const date = document.getElementById("fecha");
 const dia = document.getElementById("dia");
+const tareasNodo = document.querySelector(".tareas");
 
-let tareas = []; // array donde se guardaran las tareas ingresadas
 
-//      Contenido de fechas
+//   Contenido de fechas
     var today = new Date();
-    var fecha = today.toLocaleDateString({weekday: 'long', month: 'numeric', day:'numeric',year:'numeric'})
-    diaFecha = today.toLocaleDateString('es-MX',{weekday:'long'})
-    date.textContent = fecha
-    dia.textContent = diaFecha
+    var fecha = today.toLocaleDateString({
+    weekday: "long",
+    month: "numeric",
+    day: "numeric",
+    year: "numeric",
+    });
+    diaFecha = today.toLocaleDateString("es-MX", { weekday: "long" });
+    date.textContent = fecha;
+    dia.textContent = diaFecha;
 //
-
-
-// LocalStorage 
-
-
-
-
-//
-
-
+let tareas = [];
 
 
 function eventos() {
-    form.addEventListener("submit", validarForm);
-    
-    
+  form.addEventListener("submit", validarForm);
+  document.addEventListener('DOMContentLoaded', () => {
+    let LS = JSON.parse(localStorage.getItem("tareas")) || [];
+    tareas = LS;
+    mostrarDatos();
+  })
+
+  
 }
+
+
 
 eventos();
 
+
 function validarForm(e) {
-    e.preventDefault();
-    const tareaIngresada = document.getElementById("tarea").value;
-    
-    if (tareaIngresada === "") {
-        tareaVacia.textContent = 'Tarea Vacia'
-        setTimeout(() => {
-            tareaVacia.textContent = ''
-            
-        }, 3000);
-        return
-    }
+  e.preventDefault();
+  const tarea = document.getElementById("tarea").value;
 
-        const objTarea = {
-        tarea:tareaIngresada,
-        estado:false,
-        }
-        
-        tareas = [objTarea]
-        
-        form.reset();
-        mostrarDatos();
+  if (tarea === "") {
+    tareaVacia.textContent = "Tarea Vacia";
+    setTimeout(() => {
+      tareaVacia.textContent = "";
+    }, 3000);
+    return;
+  }
 
+  const objTarea = {
+    tarea: tarea,
+    id: Date.now(), // es el tiempo (milisegundos) que pasan desde 1970 hasta el momento donde se agregue la tarea
+  };
+
+  tareas.push(objTarea)
+
+
+  
+  form.reset();
+  mostrarDatos();
+  
+}
+
+function mostrarDatos() {
+  limpiarDatos();
+
+  if (tareas.length === "") {
+    const mensaje = document.createElement("h5");
+    mensaje.textContent = "Sin Tareas";
+    tareas.appendChild(mensaje);
+    return;
+  }
+
+  tareas.forEach((element) => {
+    const listaTareas = document.createElement("div");
+    listaTareas.classList.add("item-tarea");
+
+    listaTareas.innerHTML = `<p class="ctarea" tarea-id="${element.id}">${element.tarea}</p>`;
+
+    tareasNodo.appendChild(listaTareas);
+  });
+
+
+    localStorage.setItem('tareas', JSON.stringify(tareas))
+  
 }
 
 
 
-function mostrarDatos() {
+  
 
-    tareas.forEach((element) =>{
-        const listaTareas = document.createElement("div");
-        listaTareas.classList.add("item-tarea");
 
-            listaTareas.innerHTML = 
-            `<p class="ctarea">${element.tarea}</p>`
-        
-        
-
-        const tareasNodo = document.querySelector(".tareas")       
-        tareasNodo.appendChild(listaTareas);
-        
-    })
-    
+function limpiarDatos() {
+  tareasNodo.innerHTML = "";
 }
